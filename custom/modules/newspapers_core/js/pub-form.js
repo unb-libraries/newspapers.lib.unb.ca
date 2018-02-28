b1db5e5d67c4161f36683fbfe44c19414afe1ab8
JeremyMcDermott <jeremy.mcdermott@unb.ca>
2018-02-28 16:32:57 -0400

    Feature parity for first/last issue JS library

--- custom/modules/newspapers_core/js/pub-form.js
+++ custom/modules/newspapers_core/js/pub-form.js
@@ -34,18 +34,21 @@ function lastIssueDateRange() {
     if (jQuery("#edit-field-last-issue-date-is-approx-value").is(":checked")) {
         jQuery("#edit-field-last-issue-date-0 .panel-title").text("Last Issue Date Range");
         jQuery("#edit-field-last-issue-date-0 label:first-child").text("Approximate start date");
-        jQuery("#edit-field-last-issue-date-0-value + label").text("Approximate end date");
-        jQuery("#edit-field-last-issue-date-0-value + label").slideDown(300);
+        jQuery("#edit-field-last-issue-date-0-value + label").text("Approximate end date").slideDown(300);
         jQuery("#edit-field-last-issue-date-0-end-value-date").slideDown(300);
     } else {
         jQuery("#edit-field-last-issue-date-0 .panel-title").text("Last Issue Date");
         jQuery("#edit-field-last-issue-date-0 label:first-child").text("Date");
-        jQuery("#edit-field-last-issue-date-0-value + label").slideUp(200);
-        jQuery("#edit-field-last-issue-date-0-end-value-date").slideUp(200);
         /**
          * Clear the end date widget when Approximate checkbox is unchecked.
          */
         jQuery('#edit-field-last-issue-date-0-end-value-date').val('');
+        jQuery("#edit-field-last-issue-date-0-value + label").slideUp(200);
+        jQuery("#edit-field-last-issue-date-0-end-value-date").slideUp(200);
+        /**
+         * Remove 'value should not be null' validation alert if 'First Issue is Approximate' is unchecked.
+         */
+        jQuery("#edit-field-last-issue-date-wrapper .alert-danger").slideUp();
     }
 }
 
