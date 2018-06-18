54aa93825f2b5f86fe9a0085fadc8e9ce55561f8
jsanford <jsanford@unb.ca>
2018-06-18 14:06:33 -0300

    NBNP-2 Add holding type ID helper

--- custom/modules/serials/modules/serial_holding/src/TaxonomyHelper.php
+++ custom/modules/serials/modules/serial_holding/src/TaxonomyHelper.php
@@ -27,4 +27,41 @@ class TaxonomyHelper {
     }
   }
 
+  /**
+   * Gets the current Holding types.
+   *
+   * @throws \Exception
+   *
+   * @return \Drupal\taxonomy\TermInterface[]
+   *   An array of TermInterface objects that describe holding types.
+   */
+  public static function getHoldingTypes() {
+    return \Drupal::entityTypeManager()
+      ->getStorage('taxonomy_term')
+      ->loadByProperties(
+        [
+          'vid' => 'serial_holding_types',
+        ]
+      );
+  }
+
+  /**
+   * Gets the tid of a holding type if it exists.
+   *
+   * @throws \Exception
+   *
+   * @return int
+   *   The tid of the holding type, 0 otherwise.
+   */
+  public static function getHoldingTermId($type_string) {
+    $object = new static();
+    $types = $object->getHoldingTypes();
+    foreach ($types as $type) {
+      if ($type->getName() == $type_string) {
+        return $type->id();
+      }
+    }
+    return 0;
+  }
+
 }
