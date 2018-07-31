50c12cd2ce4315396be3ed6e4c72697f01e23358
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2018-07-31 11:00:17 -0300

    NBNP-35 Use new CSV date cols to migrate 1st/last dates

--- tests/visual-regression/visreg_content/src/Event/MicroformsTitleMigrateEvent.php
+++ tests/visual-regression/visreg_content/src/Event/MicroformsTitleMigrateEvent.php
@@ -44,6 +44,51 @@ class MicroformsTitleMigrateEvent implements EventSubscriberInterface {
       $row->setSourceProperty('country_code', $address_country);
       $row->setSourceProperty('province', $address_administrative_area);
       $row->setSourceProperty('city', $address_city);
+
+      // Borrowed code from TitleMigrateEvent.
+      $first_issue_date_type = $first_issue_verbatim_date = $first_issue_end_date = NULL;
+      $first_issue_start_date = trim($row->getSourceProperty('first_issue_start_date'));
+      list($first_start_day, $first_start_month, $first_start_year) = explode("/", $first_issue_start_date);
+      if (checkdate((int) $first_start_month, (int) $first_start_day, (int) $first_start_year)) {
+        $first_issue_end_date = trim($row->getSourceProperty('first_issue_end_date'));
+        list($first_end_day, $first_end_month, $first_end_year) = explode("/", $first_issue_end_date);
+        if (checkdate((int) $first_end_month, (int) $first_end_day, (int) $first_end_year)) {
+          $first_issue_date_type = "approximate";
+          $first_issue_verbatim_date = trim($row->getSourceProperty('first_issue_date'));
+        }
+        else {
+          $first_issue_date_type = "exact";
+        }
+      }
+      $first_issue_date_range = $first_issue_start_date . "-" . $first_issue_end_date;
+      $row->setSourceProperty('first_issue_date_type', $first_issue_date_type);
+      $row->setSourceProperty('first_issue_date_range', $first_issue_date_range);
+      $row->setSourceProperty('first_issue_verbatim_date', $first_issue_verbatim_date);
+
+      $last_issue_date_type = $last_issue_end_date = NULL;
+      $last_issue_start_date = trim($row->getSourceProperty('last_issue_start_date'));
+      $last_issue_verbatim_date = strtolower(trim($row->getSourceProperty('last_issue_date')));
+      list($last_start_day, $last_start_month, $last_start_year) = explode("/", $last_issue_start_date);
+      if ($last_issue_verbatim_date == "present") {
+        $last_issue_date_type = "ongoing";
+        $last_issue_verbatim_date = NULL;
+      }
+      elseif (checkdate((int) $last_start_month, (int) $last_start_day, (int) $last_start_year)) {
+        $last_issue_end_date = trim($row->getSourceProperty('last_issue_end_date'));
+        list($last_end_day, $last_end_month, $last_end_year) = explode("/", $last_issue_end_date);
+        if (checkdate((int) $last_end_month, (int) $last_end_day, (int) $last_end_year)) {
+          $last_issue_date_type = "approximate";
+          $last_issue_verbatim_date = trim($row->getSourceProperty('last_issue_date'));
+        }
+        else {
+          $last_issue_date_type = "exact";
+          $last_issue_verbatim_date = NULL;
+        }
+      }
+      $last_issue_date_range = $last_issue_start_date . "-" . $last_issue_end_date;
+      $row->setSourceProperty('last_issue_date_type', $last_issue_date_type);
+      $row->setSourceProperty('last_issue_date_range', $last_issue_date_range);
+      $row->setSourceProperty('last_issue_verbatim_date', $last_issue_verbatim_date);
     }
 
   }
