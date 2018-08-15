c97e08091e8119b29236f9c069eb09e64b77c063
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2018-08-15 09:39:24 -0300

    NBNP-45 Increase specificity on date input selector

--- custom/modules/newspapers_core/js/newspapers_core.views_exposed_filters.js
+++ custom/modules/newspapers_core/js/newspapers_core.views_exposed_filters.js
@@ -4,7 +4,7 @@
     Drupal.behaviors.inputDateWidget = {
         attach: function (context, settings) {
             // Workaround: change Exposed filters type from "text' to 'date' for Views date fields.
-            $("input[name*='issue_date']").prop("type", "date");
+            $(".views-exposed-filters input[name*='issue_date']").prop("type", "date");
         },
     };
 })(jQuery);
