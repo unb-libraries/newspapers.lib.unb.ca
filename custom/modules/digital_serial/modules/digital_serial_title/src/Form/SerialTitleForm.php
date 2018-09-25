dd75df0520b78aa85345dfa009a7294c1773cf3c
jsanford <jsanford@unb.ca>
2018-09-25 14:23:06 -0300

    NBNP-25 Update messaging on save/add of title

--- custom/modules/digital_serial/modules/digital_serial_title/src/Form/SerialTitleForm.php
+++ custom/modules/digital_serial/modules/digital_serial_title/src/Form/SerialTitleForm.php
@@ -4,6 +4,7 @@ namespace Drupal\digital_serial_title\Form;
 
 use Drupal\Core\Entity\ContentEntityForm;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\digital_serial_title\Entity\SerialTitle;
 
 /**
  * Form controller for Digital Serial Title edit forms.
@@ -15,36 +16,65 @@ class SerialTitleForm extends ContentEntityForm {
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
+  public function save(array $form, FormStateInterface $form_state) {
     /* @var $entity \Drupal\digital_serial_title\Entity\SerialTitle */
-    $form = parent::buildForm($form, $form_state);
+    $entity = &$this->entity;
+    $this->saveAndReportSaveAction($form, $form_state, $entity);
 
-    $entity = $this->entity;
-
-    return $form;
+    $form_state->setRedirect(
+      'entity.digital_serial_title.canonical',
+      [
+        'digital_serial_title' => $entity->id(),
+      ]
+    );
   }
 
   /**
-   * {@inheritdoc}
+   * Report the entity save action to the user.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   * @param int $status
+   *   The status of the entity save operation.
+   * @param \Drupal\digital_serial_title\Entity\SerialTitle $entity
+   *   The entity that was saved.
    */
-  public function save(array $form, FormStateInterface $form_state) {
-    $entity = &$this->entity;
-
-    $status = parent::save($form, $form_state);
-
+  private function reportSaveAction(array $form, FormStateInterface $form_state, $status, SerialTitle $entity) {
     switch ($status) {
       case SAVED_NEW:
-        drupal_set_message($this->t('Created the %label Digital Serial Title.', [
-          '%label' => $entity->label(),
-        ]));
+        $op = 'Created';
         break;
 
       default:
-        drupal_set_message($this->t('Saved the %label Digital Serial Title.', [
-          '%label' => $entity->label(),
-        ]));
+        $op = 'Saved';
     }
-    $form_state->setRedirect('entity.digital_serial_title.canonical', ['digital_serial_title' => $entity->id()]);
+
+    $this->messenger()->addMessage(
+      $this->t(
+        '%op the %label Serial Title.',
+        [
+          '%op' => $op,
+          '%label' => $entity->label(),
+        ]
+      )
+    );
+  }
+
+  /**
+   * Save the entity and report the action to the user.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   * @param \Drupal\digital_serial_title\Entity\SerialTitle $entity
+   *   The entity that was saved.
+   */
+  private function saveAndReportSaveAction(array $form, FormStateInterface $form_state, SerialTitle $entity) {
+    $status = parent::save($form, $form_state);
+    $this->reportSaveAction($form, $form_state, $status, $entity);
   }
 
 }
