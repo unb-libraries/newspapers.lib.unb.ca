d765032d64431572ee51e0603fd325e1747139dd
jsanford <jsanford@unb.ca>
2018-09-26 11:01:04 -0300

    NBNP-47 Add publication date slider on title search

---
+++ custom/modules/newspapers_core/src/Plugin/search_api/processor/IndexAdditionalTitleInfo.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Drupal\newspapers_core\Plugin\search_api\processor;
+
+use DateInterval;
+use DatePeriod;
+use DateTime;
+use Drupal\node\NodeInterface;
+use Drupal\search_api\Datasource\DatasourceInterface;
+use Drupal\search_api\IndexInterface;
+use Drupal\search_api\Item\ItemInterface;
+use Drupal\search_api\Processor\ProcessorPluginBase;
+use Drupal\search_api\Processor\ProcessorProperty;
+
+/**
+ * Adds additional information to indexed publications.
+ *
+ * @SearchApiProcessor(
+ *   id = "index_parent_title_info",
+ *   label = @Translation("Index Additional Information for a Publication"),
+ *   description = @Translation("Add a additional title information to index."),
+ *   stages = {
+ *     "add_properties" = 0,
+ *   },
+ *   locked = true,
+ *   hidden = true,
+ * )
+ */
+class IndexAdditionalTitleInfo extends ProcessorPluginBase {
+
+  /**
+   * Only enabled for an index that indexes the publication entity.
+   *
+   * {@inheritdoc}
+   */
+  public static function supportsIndex(IndexInterface $index) {
+    foreach ($index->getDatasources() as $datasource) {
+      if ($datasource->getEntityTypeId() == 'node') {
+        return TRUE;
+      }
+    }
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPropertyDefinitions(DatasourceInterface $datasource = NULL) {
+    $properties = [];
+
+    if (!$datasource) {
+      $definition = [
+        'label' => $this->t('Publication Years'),
+        'description' => $this->t('Years this publication was published.'),
+        'type' => 'integer',
+        'is_list' => TRUE,
+        'processor_id' => $this->getPluginId(),
+      ];
+      $properties['years_published'] = new ProcessorProperty($definition);
+    }
+
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addFieldValues(ItemInterface $item) {
+    $node = $item->getOriginalObject()->getValue();
+    if ($node instanceof NodeInterface) {
+      if ($node->bundle() == 'publication') {
+
+        // Years published.
+        $fields = $this->getFieldsHelper()
+          ->filterForPropertyPath($item->getFields(), NULL, 'years_published');
+        foreach ($fields as $field) {
+          if (!empty($node->get('field_first_issue_search_date')->date) && !empty($node->get('field_last_issue_search_date')->date)) {
+            $pub_start = new DateTime($node->get('field_first_issue_search_date')->date->format('Y-m-d'));
+            $pub_end = new DateTime($node->get('field_last_issue_search_date')->date->format('Y-m-d'));
+            $interval = new DateInterval('P1Y');
+            $period = new DatePeriod($pub_start, $interval, $pub_end);
+
+            foreach ($period as $dt) {
+              $field->addValue($dt->format("Y"));
+            }
+          }
+        }
+      }
+    }
+  }
+
+}
