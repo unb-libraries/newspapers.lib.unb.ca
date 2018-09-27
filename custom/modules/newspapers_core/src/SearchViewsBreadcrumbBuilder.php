9db1803ffca37ef55716eb3c0f3db4721be7e810
jsanford <jsanford@unb.ca>
2018-09-27 08:02:28 -0300

    NBNP-133 Add 'Newspapers' to search page breadcrumbs

---
+++ custom/modules/newspapers_core/src/SearchViewsBreadcrumbBuilder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Drupal\newspapers_core;
+
+use Drupal\Core\Breadcrumb\Breadcrumb;
+use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Url;
+
+/**
+ * Breadcrumb builder for search views.
+ *
+ * @package Drupal\newspapers_core
+ */
+class SearchViewsBreadcrumbBuilder implements BreadcrumbBuilderInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    return in_array(
+      $route_match->getRouteName(),
+      [
+        'view.digital_page_lister.page_search',
+        'view.search.search_page',
+      ]
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(RouteMatchInterface $route_match) {
+    $breadcrumb = new Breadcrumb();
+    $breadcrumb->addCacheContexts(["url"]);
+
+    $breadcrumb->addLink(
+      Link::fromTextAndUrl(
+        $this->t('UNB Libraries'),
+        Url::fromUri('https://lib.unb.ca')
+      )
+    );
+
+    $breadcrumb->addLink(
+      Link::createFromRoute(
+        $this->t('Newspapers'),
+        '<front>'
+      )
+    );
+
+    return $breadcrumb;
+  }
+
+}
