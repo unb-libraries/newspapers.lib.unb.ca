be57361f101deda03812a6454c99e3327affc9f3
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2018-10-09 09:13:06 -0300

    NBNP-95 Customize views exposed filters, add toggle + state logic

--- custom/modules/newspapers_core/js/newspapers_core.views_exposed_filters.js
+++ custom/modules/newspapers_core/js/newspapers_core.views_exposed_filters.js
@@ -5,6 +5,22 @@
         attach: function (context, settings) {
             // Workaround: change Exposed filters type from "text' to 'date' for Views date fields.
             $(".views-exposed-filters input[name*='issue_date']").prop("type", "date");
+
+            $("#toggle-widget").click(function () {
+                if (this.checked) {
+                    $("input#toggle-widget").attr("checked", true);
+                    $(".filters").show(300);
+                } else {
+                    $("input#toggle-widget").removeAttr("checked");
+                    $(".filters").hide(300);
+                }
+            });
+            // Browser back button checkbox state fix.
+            if ($("#toggle-widget").is(":checked")) {
+                $(".filters").show();
+            } else {
+                $(".filters").hide();
+            }
         },
     };
 })(jQuery);
