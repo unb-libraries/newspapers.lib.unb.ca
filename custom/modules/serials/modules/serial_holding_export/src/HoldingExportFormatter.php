05f60028c0134219db9091f368d925a7534bb831
jsanford <jsanford@unb.ca>
2018-10-26 12:45:34 -0300

    NBNP-151 Finalize initial holdings export

--- custom/modules/serials/modules/serial_holding_export/src/HoldingExportFormatter.php
+++ custom/modules/serials/modules/serial_holding_export/src/HoldingExportFormatter.php
@@ -10,6 +10,8 @@ use Drupal\node\NodeInterface;
  */
 class HoldingExportFormatter {
 
+  const BASE_URL = 'https://newspapers.lib.unb.ca';
+
   /**
    * An associative array of configuration for this holding.
    *
@@ -96,6 +98,72 @@ class HoldingExportFormatter {
    */
   public function getPublicationTitle() {
     return $this->publication->getTitle();
+  }
+
+  /**
+   * Formatter callback for publication issn.
+   */
+  public function getPublicationIssn() {
+    return $this->publication->get('field_issn')->getString();
+  }
+
+  /**
+   * Formatter callback for holding start date.
+   */
+  public function getHoldingStartDate() {
+    if (!empty($this->holding->get('holding_start_date')->date)) {
+      return $this->formatHoldingDate(
+        $this->holding->get('holding_start_date')->date
+      );
+    }
+    return NULL;
+  }
+
+  /**
+   * Formatter callback for holding end date.
+   */
+  public function getHoldingEndDate() {
+    if (!empty($this->holding->get('holding_end_date')->date)) {
+      return $this->formatHoldingDate(
+        $this->holding->get('holding_end_date')->date
+      );
+    }
+    return NULL;
+  }
+
+  /**
+   * Format date strings.
+   */
+  private function formatHoldingDate($date) {
+    return $date->format('Y-m-d');
+  }
+
+  /**
+   * Formatter callback for publication URL.
+   */
+  public function getPublicationUrl() {
+    return self::BASE_URL . $this->publication->toUrl()->toString();
+  }
+
+  /**
+   * Formatter callback for coverage notes.
+   */
+  public function getCoverageNotes() {
+    return $this->holding->getHoldingCoverage();
+  }
+
+  /**
+   * Formatter callback for holding location.
+   */
+  public function getHoldingLocation() {
+    return $this->holding->getHoldingLocation();
+  }
+
+  /**
+   * Formatter callback for OCLC number.
+   */
+  public function getPublicationOclcNumber() {
+    return $this->publication->get('field_oclc')->getString();
   }
 
 }
