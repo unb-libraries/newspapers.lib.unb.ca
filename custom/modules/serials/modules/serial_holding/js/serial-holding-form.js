d454398634e122cc2f63d63872b4318eacc4ea3f
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2018-11-05 11:53:21 -0400

    NBNP-201 Fix Drupal core 'required' handling (issue #2855139)

---
+++ custom/modules/serials/modules/serial_holding/js/serial-holding-form.js
@@ -0,0 +1,27 @@
+(function ($, Drupal) {
+
+// Fix core's required handling (https://www.drupal.org/project/drupal/issues/2855139).
+    $(document).bind('state:required', function (e) {
+        if (e.trigger) {
+            var fields = $(e.target).find('input, select, textarea, fieldset');
+            fields.each(function() {
+                var label = 'label' + (this.id ? '[for=' + this.id + ']' : '');
+                var $field_labels = $(e.target).find(label);
+                var $fieldset_legends = $(e.target).find('legend span.fieldset-legend');
+                var $labels = $field_labels.add($fieldset_legends);
+                if (e.value) {
+                    $(this).attr({ required: 'required', 'aria-required': 'aria-required' });
+                    $labels.each(function() {
+                        $(this).addClass('js-form-required form-required');
+                    });
+                } else {
+                    $(this).removeAttr('required aria-required');
+                    $labels.each(function() {
+                        $(this).removeClass('js-form-required form-required');
+                    });
+                }
+            })
+        }
+    });
+
+})(jQuery, Drupal);
