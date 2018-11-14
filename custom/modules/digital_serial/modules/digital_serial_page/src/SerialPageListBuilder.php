038fe10026f09ff04dcd579e049031ea3871e3f4
jsanford <jsanford@unb.ca>
2018-11-14 10:26:43 -0400

    NBNP-222 Remove erroneously pasted second query

--- custom/modules/digital_serial/modules/digital_serial_page/src/SerialPageListBuilder.php
+++ custom/modules/digital_serial/modules/digital_serial_page/src/SerialPageListBuilder.php
@@ -48,9 +48,6 @@ class SerialPageListBuilder extends EntityListBuilder {
       ->condition('parent_issue', $issue)
       ->sort('page_sort');
 
-    $query = \Drupal::entityQuery('serial_holding')
-      ->condition('parent_title', $this->id());
-
     // Only add the pager if a limit is specified.
     if ($this->limit) {
       $query->pager($this->limit);
