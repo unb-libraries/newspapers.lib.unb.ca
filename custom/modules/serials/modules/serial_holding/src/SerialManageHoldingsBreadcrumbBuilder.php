19ea8457b52d96279ab060e98631a287459d4b0a
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2018-11-15 14:25:01 -0400

    NBNP-228 Add Manage Holdings breadcrumb class+service

---
+++ custom/modules/serials/modules/serial_holding/src/SerialManageHoldingsBreadcrumbBuilder.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\serial_holding;
+
+use Drupal\Core\Breadcrumb\Breadcrumb;
+use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Url;
+
+/**
+ * Breadcrumb builder for manage holdings.
+ */
+class SerialManageHoldingsBreadcrumbBuilder implements BreadcrumbBuilderInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    $route = $route_match->getRouteName();
+    $applies = [
+      'serial_holding.manage_serial_holdings',
+    ];
+    return in_array($route, $applies);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(RouteMatchInterface $route_match) {
+    $nid = $route_match->getParameter('node');
+    $node = node_load($nid);
+    $node_title = $node->getTitle();
+
+    // Set breadcrumb.
+    $breadcrumb = new Breadcrumb();
+
+    // Set links;
+    $links = [
+      Link::fromTextAndUrl(
+        $this->t('UNB Libraries'),
+        Url::fromUri('https://lib.unb.ca')
+      ),
+      Link::createFromRoute(
+        $this->t('Newspapers'),
+        '<front>'
+      ),
+      Link::createFromRoute(
+        $node_title,
+        'entity.node.canonical',
+        ['node' => $nid]
+      ),
+    ];
+    $breadcrumb->setLinks($links);
+
+    return $breadcrumb;
+  }
+
+}
