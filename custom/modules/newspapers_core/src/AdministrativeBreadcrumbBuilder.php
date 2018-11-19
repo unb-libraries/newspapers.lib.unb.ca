46228347c19fe3635f81a4536e3647b1211a901e
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2018-11-19 14:03:57 -0400

    NBNP-231 Add breadcrumb class for Drupal administrative paths

---
+++ custom/modules/newspapers_core/src/AdministrativeBreadcrumbBuilder.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\newspapers_core;
+
+use Drupal\Core\Breadcrumb\Breadcrumb;
+use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Url;
+
+/**
+ * Class Breadcrumbs.
+ *
+ * @package Drupal\modulename
+ */
+class AdministrativeBreadcrumbBuilder implements BreadcrumbBuilderInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    $current_route = $route_match->getRouteName();
+    $paths = [
+      'entity.user.',
+      'node.add',
+      'system.',
+    ];
+
+    foreach ($paths as $path) {
+      if (strpos($current_route, $path) !== FALSE) {
+        return TRUE;
+      }
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(RouteMatchInterface $route_match) {
+    $breadcrumb = new Breadcrumb();
+
+    $breadcrumb->addLink(
+      Link::fromTextAndUrl(
+        $this->t('UNB Libraries'),
+        Url::fromUri('https://lib.unb.ca')
+      )
+    );
+
+    $breadcrumb->addLink(
+      Link::createFromRoute(
+        $this->t('Newspapers'),
+        '<front>'
+      )
+    );
+
+    return $breadcrumb;
+  }
+
+}
