7ae25dcea47b45d98ddd4ca0abfa0da0af4478a3
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2018-11-23 12:06:52 -0400

    NBNP-211 Create export holdings controller

---
+++ custom/modules/newspapers_core/src/Controller/ExportHoldingsController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\newspapers_core\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Provides route responses for the Example module.
+ */
+class ExportHoldingsController extends ControllerBase {
+
+  /**
+   * Returns a simple page.
+   *
+   * @return array
+   *   A simple renderable array.
+   */
+  public function exportHoldings() {
+    $element = [
+      '#markup' => '<dl>
+      <dt>Microfilm</dt>
+      <dd><a href="/serials/holdings/export/Microfilm">https://newspapers.lib.unb.ca/serials/holdings/export/Microfilm</a></dd>
+      <dt>Print</dt>
+      <dd><a href="/serials/holdings/export/Print">https://newspapers.lib.unb.ca/serials/holdings/export/Print</a></dd>
+      <dt>Digital</dt>
+      <dd><a href="/serials/holdings/export/Digital">https://newspapers.lib.unb.ca/serials/holdings/export/Digital</a></dd>
+      <dt></a>Online</dt>
+      <dd><a href="/serials/holdings/export/Online">https://newspapers.lib.unb.ca/serials/holdings/export/Online</a></dd>
+      </dl>',
+    ];
+    return $element;
+  }
+
+}
