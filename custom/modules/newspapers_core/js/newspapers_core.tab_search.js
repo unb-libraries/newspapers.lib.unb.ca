80499a0dd624056a64409fa532650e410cde4385
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2018-12-12 15:29:46 -0400

    NBNP-260 Enhancement: autofocus search input on load+tab switch

--- custom/modules/newspapers_core/js/newspapers_core.tab_search.js
+++ custom/modules/newspapers_core/js/newspapers_core.tab_search.js
@@ -3,7 +3,13 @@
 
     Drupal.behaviors.multiSubmit = {
         attach: function (context, settings) {
-            // Simulate click function when user presses enter key.
+            // Autofocus the search input of active tab.
+            $('.search-form input:visible').focus();
+            $('.tab a[data-toggle="tab"]').on('shown.bs.tab', function() {
+                $('.search-form input:visible').focus();
+            });
+
+            // Simuate click function when user presses enter key.
             $("input").on("keypress", function (e) {
                 if (e.keyCode == 13) {
                     $(".search-form button[type=submit]:visible").focus();
