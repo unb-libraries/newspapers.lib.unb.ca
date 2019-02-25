5b64d1730ec4dd251dfe147c7cb3747622b683bf
jsanford <jsanford@unb.ca>
2019-02-25 15:13:22 -0400

    DOCKW-2 Use updated ULI script when reporting URI

--- scripts/container/pre-init.d/99_z_notify_user_URI.sh
+++ scripts/container/pre-init.d/99_z_notify_user_URI.sh
@@ -3,5 +3,5 @@ if [ -n "$DEV_WEB_URI" ] && [ "$DEPLOY_ENV" = "local" ]; then
   echo "Visit your instance at:"
   echo "$DEV_WEB_URI"
   echo "Log-in to your instance via:"
-  drush --root=${DRUPAL_ROOT} --uri=default --yes uli | sed -e "s|http://default|$DEV_WEB_URI|g"
+  /scripts/drupalUli.sh | sed -e "s|http://default|$DEV_WEB_URI|g"
 fi
