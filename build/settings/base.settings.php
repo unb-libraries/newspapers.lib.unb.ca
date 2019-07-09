4ea7e77dd408521ac1ce6d7b0180e3f7de783a98
jsanford <jsanford@unb.ca>
2019-07-09 12:40:01 -0300

    Changes related to upstream settings management

--- build/settings/base.settings.php
+++ build/settings/base.settings.php
@@ -5,15 +5,6 @@
  * Include global settings overrides here.
  */
 
-// Environment based includes.
-if (isset($_SERVER['APPLICATION_ENV'])) {
-  $environment = strtolower($_SERVER['APPLICATION_ENV']);
-  $environment_include = dirname(__FILE__) . "/settings.$environment.inc";
-  if (file_exists($environment_include)) {
-    require_once $environment_include;
-  }
-}
-
 // Redis.
 $settings['cache_prefix']['default'] = 'newspapers_';
 $conf['chq_redis_cache_enabled'] = TRUE;
