b8ac9c47bbac740cb9dc4ce5b73accfb50d94aa6
Jacob Sanford <jsanford@unb.ca>
2021-10-01 13:27:34 -0300

    Set redis cache prefix based on SITE_ID

--- build/settings/base.settings.php
+++ build/settings/base.settings.php
@@ -9,7 +9,7 @@
 $settings['install_profile'] = 'minimal';
 
 // Redis.
-$settings['cache_prefix']['default'] = 'newspapers_';
+$settings['cache_prefix']['default'] = 'DRUPAL_SITE_ID_';
 $conf['chq_redis_cache_enabled'] = TRUE;
 require_once dirname(__FILE__) . "/settings.redis.inc";
 
