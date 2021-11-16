6a0ad223c006922e81b585bb74acbc0cd052c001
Jacob Sanford <jsanford@unb.ca>
2021-11-16 15:22:26 -0400

    Update config sync dir config to tokenized value

--- build/settings/base.settings.php
+++ build/settings/base.settings.php
@@ -18,4 +18,4 @@ if (extension_loaded('newrelic')) {
   require_once dirname(__FILE__) . "/settings.newrelic.inc";
 }
 
-$settings['config_sync_directory'] = '../configuration';
+$settings['config_sync_directory'] = 'DRUPAL_CONFIGURATION_DIR';
