50ba078158a3f71f4e57cdca023c6f3a5b5bfc71
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2022-09-22 11:19:41 -0300

    NBNP-395 Tweak whitespace vs a vis middle breakpoints

--- custom/modules/newspapers_core/src/Plugin/Block/PartnershipsBlockController.php
+++ custom/modules/newspapers_core/src/Plugin/Block/PartnershipsBlockController.php
@@ -37,16 +37,16 @@ class PartnershipsBlockController extends BlockBase {
    */
   protected function getValue() {
     $html = '
-    <div class="d-flex flex-column flex-sm-row mx-0 mx-sm-5">
-      <div class="mx-5 mb-3 mb-sm-0">
+    <div class="d-flex flex-column flex-sm-row mx-5 mx-sm-0 mx-lg-5">
+      <div class="mx-5 mx-sm-0 mx-lg-5 mb-3 mb-sm-0">
         <a href="https://lib.unb.ca"><img src="/themes/custom/newspapers_lib_unb_ca/src/img/unb_libraries_logo.png"
         alt="UNB Libraries"/></a>
       </div>
-      <div class="mx-5 my-3 my-sm-0">
+      <div class="mx-5 mx-sm-0 mx-lg-5 my-3 my-sm-0">
         <a href="https://archives.gnb.ca/Archives"><img src="/themes/custom/newspapers_lib_unb_ca/src/img/gnb_logo.png"
         alt="Government of New Brunswick, Canada|Gouvernement du Nouveau-Brunswick, Canada"/></a>
       </div>
-      <div class="mx-5 my-3 my-sm-0">
+      <div class="mx-5 mx-sm-0 mx-lg-5 my-3 my-sm-0">
         <a href="https://www.canbarchives.ca"><img src="/themes/custom/newspapers_lib_unb_ca/src/img/canb_logo.png"
         alt="Council of Archives New Brunswick|Conseil des archives Nouveau-Brunswick"/></a>
         </div>
