59d91bcb70ee5049963c5088fb58064fcacbeb81
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2023-06-20 12:43:24 -0300

    Sort more use statements

--- custom/modules/digital_serial/modules/digital_serial_issue/src/SerialIssueAccessControlHandler.php
+++ custom/modules/digital_serial/modules/digital_serial_issue/src/SerialIssueAccessControlHandler.php
@@ -2,10 +2,10 @@
 
 namespace Drupal\digital_serial_issue;
 
+use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Entity\EntityAccessControlHandler;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Session\AccountInterface;
-use Drupal\Core\Access\AccessResult;
 
 /**
  * Access controller for the Serial issue entity.
--- custom/modules/digital_serial/modules/digital_serial_page/src/SerialPageAccessControlHandler.php
+++ custom/modules/digital_serial/modules/digital_serial_page/src/SerialPageAccessControlHandler.php
@@ -2,10 +2,10 @@
 
 namespace Drupal\digital_serial_page;
 
+use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Entity\EntityAccessControlHandler;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Session\AccountInterface;
-use Drupal\Core\Access\AccessResult;
 
 /**
  * Access controller for the Serial page entity.
