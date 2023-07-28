5054a844792ddeac1ef998dc0ad5e19e637db795
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2023-07-28 09:32:39 -0300

    NBNP-415 Return boolean false value using proper case

--- custom/modules/newspapers_core/js/newspapers_core.tab_search.js
+++ custom/modules/newspapers_core/js/newspapers_core.tab_search.js
@@ -10,16 +10,16 @@
             }
 
             // Autofocus the search input of clicked tab.
-            $('.nav-tabs a[data-toggle="tab"]').on('shown.bs.tab', function () {
+            $('.nav-tabs a[data-toggle="tab"]').on("shown.bs.tab", function () {
                 $('#newspapers-core-homepage input[type="text"]:visible').focus();
             });
 
-            // Simuate click function when user presses enter key.
-            $('input[type="text"]').on('keypress', function (e) {
+            // Simulate click function when user presses enter key.
+            $('input[type="text"]').on("keypress", function (e) {
                 if (e.keyCode == 13) {
                     $('#newspapers-core-homepage input[type=submit]:visible').focus();
                     $('#newspapers-core-homepage input[type=submit]:visible').click();
-                    return FALSE;
+                    return false;
                 }
             });
         },
