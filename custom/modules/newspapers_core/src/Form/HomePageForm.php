6a04625f75be08ce2dfc006d5f03ab7e85cb5a07
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2024-01-04 10:18:17 -0400

    NBNP-434 Major frontpage form restructuring, add stats

--- custom/modules/newspapers_core/src/Form/HomePageForm.php
+++ custom/modules/newspapers_core/src/Form/HomePageForm.php
@@ -6,6 +6,7 @@ use Drupal\Core\Cache\Cache;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Link;
+use Drupal\Core\Render\Markup;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\Core\Url;
 
@@ -33,7 +34,6 @@ class HomePageForm extends FormBase {
     // Configure appropriate active tab/pane classes.
     $user_input = $form_state->getUserInput();
     $op = $user_input['op'] ?? NULL;
-    $about_tab_class = " d-sm-none";
     if ($op == 'Search FullText') {
       $title_pane_class = $about_pane_class = NULL;
       $fulltext_pane_class = "";
@@ -43,7 +43,29 @@ class HomePageForm extends FormBase {
       $title_pane_class = "show active";
     }
 
-    $form = [];
+    // Markup snippets.
+    $blurb_markup = '<p class="mb-4">The <a href="project">New Brunswick Historical Newspapers Project</a> provides
+      researchers with unified access to UNB Libraries&apos; current and historical newspaper collections in all formats,
+      from New Brunswick and across the world. Search and discover
+      <a href="/print-titles">print</a>, microform, and selected digital newspaper
+      titles available from UNB Libraries. This site also acts as an index for all known New Brunswick
+      newspapers and listing of where copies are located throughout the province and beyond.</p>
+      <p class="mb-0">For more worldwide digital newspaper content, consult
+      <a href="https://lib.unb.ca/eresources/collections/newspapers" class="external">
+      UNB Libraries licensed electronic Newspaper collections</a>. UNB/STU login required.</p>';
+
+    $searchtips_markup = '<aside class="search-tips mt-3 w-100">
+          <p class="h3 mb-1">Search Tips:</p>
+          <ul class="mb-0">
+            <li>Use uppercase AND operator to limit to all terms used. eg. <code>cattle AND Fredericton</code></li>
+            <li>Use quotation marks to do phrase searching. eg. <code>&quot;5000 pairs to select&quot;</code></li>
+            <li>After searching, use the facets to limit by date, publication and location</li>
+          </ul>
+        </aside>';
+
+    $stats_markup = $this->getStatsMarkup();
+
+    // Tab urls.
     $title_url = Url::fromUri("internal:");
     $title_link_options = [
       'attributes' => [
@@ -125,25 +147,10 @@ class HomePageForm extends FormBase {
     ];
     $about_url->setOptions($about_link_options);
 
-    $blurb = "<p>The New Brunswick Historical Newspapers Project provides researchers with
-        unified access to UNB Libraries&apos; current and historical newspaper collections in all formats, from New
-        Brunswick and across the world. Search and discover
-        <a href=\"/print-titles\">print</a>, microform, and selected digital newspaper
-        titles available from UNB Libraries. This site also acts as an index for all known New Brunswick
-        newspapers and listing of where copies are located throughout the province and beyond.</p>
-        <p>Fulltext searching is available for our growing list of
-        <a href=\"/digital-titles\">digitized titles</a>. Learn
-        more about the <a href=\"project\">New Brunswick Historical Newspapers Project</a>
-        or contact us with questions.</p>
-        <p>For more worldwide digital newspaper content, consult
-        <a href=\"https://lib.unb.ca/eresources/collections/newspapers\" class=\"external\">
-        UNB Libraries licensed electronic Newspaper collections</a>. UNB/STU login required.</p>";
+    // Tabbed form content.
+    $form = [];
 
-    $form['blurb'] = [
-      '#type' => 'markup',
-      '#markup' => "<div class=\"d-none d-sm-block bg-unb-light border mb-4 p-3 rounded\">" . $blurb . "</div>",
-    ];
-
+    // Navigation tabs.
     $form['nav-tabs'] = [
       '#type' => 'html_tag',
       '#tag' => 'ul',
@@ -158,32 +165,57 @@ class HomePageForm extends FormBase {
       ],
     ];
     $form['nav-tabs']['title'] = [
-      '#markup' => '<li class="nav-item" role="presentation">' . Link::fromTextAndUrl($this->t('Title Search'), $title_url)
-        ->toString() . '</li>',
+      '#markup' => '<li class="nav-item" role="presentation">' .
+      Link::fromTextAndUrl($this->t('Title Search'), $title_url)->toString() .
+      '</li>',
     ];
     $form['nav-tabs']['fulltext'] = [
-      '#markup' => '<li class="nav-item" role="presentation">' . Link::fromTextAndUrl($this->t('Fulltext Search'), $fulltext_url)
-        ->toString() . '</li>',
+      '#markup' => '<li class="nav-item" role="presentation">' .
+      Link::fromTextAndUrl($this->t('Fulltext Search'), $fulltext_url)->toString() .
+      '</li>',
     ];
     $form['nav-tabs']['about'] = [
-      '#markup' => '<li class="nav-item' . $about_tab_class . '" role="presentation">' . Link::fromTextAndUrl($this->t('About'), $about_url)
-        ->toString() . '</li>',
+      '#markup' => '<li class="nav-item" role="presentation">' .
+      Link::fromTextAndUrl(
+        Markup::create($this->t('About<span class="d-none d-md-inline"> the Project</span><span
+        class="fa-solid fa-circle-question ml-1"></span>')), $about_url
+      )->toString() . '</li>',
     ];
 
-    $form['tab-content'] = [
+    // Tab panel wrapper for toggleable panes & adjacent stats section.
+    $form['panel-wrapper'] = [
+      '#type' => 'container',
+      '#attributes' => [
+        'class' => [
+          'border',
+          'd-flex',
+          'flex-column',
+          'flex-lg-row',
+        ],
+        'id' => [
+          'panel-wrapper',
+        ],
+      ],
+    ];
+
+    // Wrapper for toggleable tab panes.
+    $form['panel-wrapper']['tab-content'] = [
       '#type' => 'container',
       '#attributes' => [
         'id' => [
           'search-wrapper',
         ],
         'class' => [
-          'tab-content',
-          'border',
+          'd-flex',
+          'flex-grow-1',
           'p-4',
+          'tab-content',
         ],
       ],
     ];
-    $form['tab-content']['title'] = [
+
+    // Title Search tab pane.
+    $form['panel-wrapper']['tab-content']['title'] = [
       '#type' => 'container',
       '#attributes' => [
         'class' => [
@@ -202,19 +234,19 @@ class HomePageForm extends FormBase {
         ],
       ],
     ];
-    $form['tab-content']['title']['input_title'] = [
+    $form['panel-wrapper']['tab-content']['title']['input_title'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Search for Newspaper Titles'),
-      '#description' => $this->t('Search by title, location, publisher, notes, description or combination, i.e. Moncton 1932'),
+      '#title' => $this->t('Search for newspaper titles'),
+      '#description' => $this->t('Search by title, location, publisher, notes, description or combination. eg. <code>Moncton 1932</code>'),
     ];
-    $form['tab-content']['title']['actions'] = [
+    $form['panel-wrapper']['tab-content']['title']['actions'] = [
       '#type' => 'actions',
     ];
-    $form['tab-content']['title']['actions']['submit_title'] = [
+    $form['panel-wrapper']['tab-content']['title']['actions']['submit_title'] = [
       '#type' => 'submit',
       '#value' => $this->t('Search/Browse Titles'),
     ];
-    $form['tab-content']['title']['search-scope'] = [
+    $form['panel-wrapper']['tab-content']['title']['search-scope'] = [
       '#type' => 'container',
       '#weight' => 100,
       '#attributes' => [
@@ -224,13 +256,14 @@ class HomePageForm extends FormBase {
         ],
       ],
     ];
-    $form['tab-content']['title']['search-scope']['unlimited_region'] = [
+    $form['panel-wrapper']['tab-content']['title']['search-scope']['unlimited_region'] = [
       '#type' => 'checkbox',
-      '#title' => $this->t('Include newspapers <strong>published</strong> outside of New Brunswick'),
+      '#title' => $this->t('Include newspapers published <strong>outside</strong> of New Brunswick'),
       '#default_value' => FALSE,
     ];
 
-    $form['tab-content']['fulltext'] = [
+    // Fulltext Search tab pane.
+    $form['panel-wrapper']['tab-content']['fulltext'] = [
       '#type' => 'container',
       '#attributes' => [
         'class' => [
@@ -249,48 +282,34 @@ class HomePageForm extends FormBase {
         ],
       ],
     ];
-
-    $form['tab-content']['fulltext']['search']['input_fulltext'] = [
+    $form['panel-wrapper']['tab-content']['fulltext']['search']['input_fulltext'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Search the fulltext of digitized newspapers'),
-      '#description' => '<span class="mr-1">' . $this->t('Search for keywords within the fulltext of digitized 
-        newspapers.') . '</span><a aria-controls="searchTips" aria-expanded="false" class="btn-link text-nowrap"
-        data-toggle="collapse" href="#searchTips" role="button"><span aria-hidden="true"
-        class="fa-solid fa-question-circle mr-1"></span>Search tips</a>',
+      '#description' => '<span class="mr-1">' .
+      $this->t('Search for keywords within the fulltext of digitized newspapers') . '</span>',
     ];
-    $form['tab-content']['fulltext']['search']['actions'] = [
+    $form['panel-wrapper']['tab-content']['fulltext']['search']['actions'] = [
       '#type' => 'actions',
     ];
-    $form['tab-content']['fulltext']['search']['actions']['submit_fulltext'] = [
+    $form['panel-wrapper']['tab-content']['fulltext']['search']['actions']['submit_fulltext'] = [
       '#type' => 'submit',
-      '#value' => $this->t('Search FullText'),
+      '#value' => $this->t('Search Fulltext'),
       '#attributes' => [
         'class' => [
           'btn-unb-red',
+          'px-4',
         ],
       ],
     ];
-    $form['tab-content']['fulltext']['tips'] = [
+    $form['panel-wrapper']['tab-content']['fulltext']['tips'] = [
       '#type' => 'markup',
-      '#markup' => '
-        <div class="card collapse mt-3 w-100" id="searchTips">
-          <div class="card-body">
-            <h2 class="card-title sr-only">Search Term Guidelines</h2>
-            <div class="description m-0">
-              <ul class="mb-0 px-3">
-                <li>Use uppercase AND operator to limit to all terms used. eg. <code>cattle AND Fredericton</code></li>
-                <li>Use &quot;&quot; to do phrase searching. eg. <code>&quot;5000 pairs to select&quot;</code></li>
-                <li>After searching, use the facets to limit by date, publication and location</li>
-                <li>See current listing of
-                  <a href="/digital-titles">digitally available titles</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>',
+      '#markup' => $searchtips_markup,
     ];
 
-    $form['tab-content']['about'] = [
+    // About the Project tab pane.
+    $form['panel-wrapper']['tab-content']['about'] = [
       '#type' => 'container',
+      '#weight' => 100,
       '#attributes' => [
         'class' => [
           'tab-pane',
@@ -308,12 +327,18 @@ class HomePageForm extends FormBase {
         ],
       ],
     ];
-    $form['tab-content']['about']['wrapper'] = [
+    $form['panel-wrapper']['tab-content']['about']['wrapper'] = [
       '#type' => 'container',
     ];
-    $form['tab-content']['about']['wrapper']['blurb'] = [
+    $form['panel-wrapper']['tab-content']['about']['wrapper']['blurb'] = [
       '#type' => 'markup',
-      '#markup' => $blurb,
+      '#markup' => $blurb_markup,
+    ];
+
+    // Always visible digital stats section.
+    $form['panel-wrapper']['stats'] = [
+      '#type' => 'markup',
+      '#markup' => $stats_markup,
     ];
 
     $form['#cache'] = [
@@ -335,8 +360,8 @@ class HomePageForm extends FormBase {
     $value = (string) $values['input_fulltext'];
     $op = (string) $form_state->getValue('op');
 
-    if ($op === 'Search FullText' && empty($value)) {
-      $form_state->setErrorByName('input_fulltext', $this->t('Please provide a fulltext search term'));
+    if ($op === 'Search Fulltext' && empty($value)) {
+      $form_state->setErrorByName('input_fulltext', $this->t('Please provide a fulltext search term and try again'));
     }
   }
 
@@ -346,11 +371,10 @@ class HomePageForm extends FormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $values = $form_state->getValues();
     $op = (string) $form_state->getValue('op');
-
     $input_fulltext = (string) $values['input_fulltext'];
     $input_title = (string) $values['input_title'];
 
-    if ($op === 'Search FullText') {
+    if ($op === 'Search Fulltext') {
       $query = $this->getQueryFromValue($input_fulltext);
       $form_state->setRedirectUrl(
         Url::fromUri("internal:/page-search?fulltext=$query")
@@ -390,6 +414,60 @@ class HomePageForm extends FormBase {
    */
   private function getQueryFromValue($value) {
     return (string) $value;
+  }
+
+  /**
+   * Determine the markup for the digital title stats table.
+   *
+   * @return string
+   *   The string value of the digital title stats HTML markup.
+   */
+  private function getStatsMarkup(): string {
+    // Fetch digital serial counts for the stats section.
+    $database = \Drupal::database();
+    try {
+      $pages_row_count = $database
+        ->select('digital_serial_page', 'p')
+        ->countQuery()
+        ->execute()
+        ->fetchField();
+    }
+    catch (\Exception $e) {
+      \Drupal::logger('type')->error($e->getMessage());
+      $pages_row_count = 'Unknown';
+    }
+    try {
+      $issues_row_count = $database
+        ->select('digital_serial_issue', 'i')
+        ->countQuery()
+        ->execute()
+        ->fetchField();
+    }
+    catch (\Exception $e) {
+      \Drupal::logger('type')->error($e->getMessage());
+      $issues_row_count = 'Unknown';
+    }
+    try {
+      $titles_row_count = $database
+        ->select('digital_serial_title', 't')
+        ->countQuery()
+        ->execute()
+        ->fetchField();
+    }
+    catch (\Exception $e) {
+      \Drupal::logger('type')->error($e->getMessage());
+      $titles_row_count = 'Unknown';
+    }
+
+    return '<aside class="bg-light col-lg-4 p-3 stats">' .
+      '<table class="table table-sm mb-3"><caption class="visually-hidden">Digitiazed Content</caption>' .
+      '<colgroup><col width="25%"></colgroup><tbody>' .
+      '<tr><th class="text-uppercase" scope="row">Digital<br>titles</th><td>' . number_format($titles_row_count) . '</td></tr>' .
+      '<tr><th class="text-uppercase" scope="row">Digital<br>issues</th><td>' . number_format($issues_row_count) . '</td></tr>' .
+      '<tr><th class="text-uppercase" scope="row">Total<br>pages</th><td>' . number_format($pages_row_count) . '</td></tr>' .
+      '</tbody></table>' .
+      '<p class="mb-0"><a href="/digital-titles" class="ml-1">Current list of digitized titles</a></p>' .
+      '</aside>';
   }
 
 }
