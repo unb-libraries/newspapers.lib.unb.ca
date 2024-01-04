f22c1b33a55233f48e89ebbba3c358bfbdc3eb56
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2024-01-04 10:24:12 -0400

    NBNP-434 Partnerships wrapper update

--- custom/modules/newspapers_core/src/Plugin/Block/PartnershipsBlockController.php
+++ custom/modules/newspapers_core/src/Plugin/Block/PartnershipsBlockController.php
@@ -37,14 +37,14 @@ class PartnershipsBlockController extends BlockBase {
    */
   protected function getValue() {
     $html = '
-    <div class="d-flex flex-column flex-md-row image-link-shadow justify-content-around mx-0 mx-lg-5 text-center">
-      <div class="mb-3 mb-md-0">
-        <a href="https://lib.unb.ca"><img src="/themes/custom/newspapers_lib_unb_ca/src/img/unb_libraries_logo.png"
-        alt="UNB Libraries"/></a>
-      </div>
+    <div class="d-flex flex-column flex-md-row justify-content-between mx-0 mx-lg-5 text-center">
       <div class="my-3 my-md-0">
         <a href="https://archives.gnb.ca/Archives"><img src="/themes/custom/newspapers_lib_unb_ca/src/img/gnb_logo.png"
         alt="Government of New Brunswick, Canada|Gouvernement du Nouveau-Brunswick, Canada"/></a>
+      </div>
+      <div class="my-3 my-md-0">
+        <a href="https://lib.unb.ca"><img src="/themes/custom/newspapers_lib_unb_ca/src/img/unb_libraries_logo.png"
+        alt="UNB Libraries"/></a>
       </div>
       <div class="my-3 my-md-0">
         <a href="https://www.canbarchives.ca"><img src="/themes/custom/newspapers_lib_unb_ca/src/img/canb_logo.png"
