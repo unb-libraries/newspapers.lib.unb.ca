efa6cc09ac5f3f37d31d39c5e81f5f356de60bfb
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2024-03-06 12:04:50 -0400

    Do not test for 'alert' class on fulltext page heading

--- tests/cypress/status/e2e/newspapers-lib-unb-ca.cy.js
+++ tests/cypress/status/e2e/newspapers-lib-unb-ca.cy.js
@@ -32,7 +32,7 @@ describe('New Brunswick Historical Newspapers Project', {baseUrl: host, groups: 
         .click()
       cy.url()
         .should('match', /\/page-search\?fulltext=/)
-      cy.get('h2.alert-success')
+      cy.get('.view-header h2')
         .should('contain', 'Displaying 1 -')
       cy.get('.search-results .views-row')
         .should('have.lengthOf.at.least', 10)
