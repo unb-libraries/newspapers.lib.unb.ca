ac9ca140b4262d4f21cc0e18290ce656f3f86fa4
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2024-04-19 08:30:23 -0300

    NBNP-125 Update deprecated keypress DOM event code

--- custom/modules/newspapers_core/js/newspapers_core.tab_search.js
+++ custom/modules/newspapers_core/js/newspapers_core.tab_search.js
@@ -3,20 +3,14 @@
 
     Drupal.behaviors.multiSubmit = {
         attach: function () {
-            // On empty fulltext input error: switch to Fulltext tab + focus input.
-            if ($('div[aria-label="Error message"]').length) {
-                $('a#tab-fulltext').click();
-                $('#newspapers-core-homepage input[type="search"]:visible').focus();
-            }
-
             // Autofocus the search input of clicked tab.
             $('.nav-tabs a[data-toggle="tab"]').on("shown.bs.tab", function () {
                 $('#newspapers-core-homepage input[type="search"]:visible').focus();
             });
 
             // Simulate click function when user presses enter key.
-            $('input[type="search"]').on("keypress", function (e) {
-                if (e.keyCode == 13) {
+            $('input[type="search"]').on("keypress", function (event) {
+                if (event.key === 'Enter') {
                     $('#newspapers-core-homepage input[type=submit]:visible').focus();
                     $('#newspapers-core-homepage input[type=submit]:visible').click();
                     return false;
