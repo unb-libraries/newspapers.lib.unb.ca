bd23805e55aed3a5d0f76fbaa99694ac1bf61df9
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2024-05-09 12:11:37 -0300

    NBNP-446 Document facet throbber JS asset, remove Drupal dependency

--- custom/modules/newspapers_core/js/newspapers_core.facet_loading.js
+++ custom/modules/newspapers_core/js/newspapers_core.facet_loading.js
@@ -1,12 +1,13 @@
-(function ($, Drupal) {
-    Drupal.behaviors.facetAnimation = {
-        attach: function () {
-            $('.facets-widget-checkbox .facet-item').click(function(e) {
-                if (e.originalEvent !== undefined) {
-                    $('label img', this).toggleClass('d-none');
-                    $('input', this).click();
-                }
-            });
-        },
-    };
-})(jQuery, Drupal);
+/**
+ * @file
+ * Show animated throbber image on facet checkbox widget > item click.
+ * Also extends clickable area of facet item to improve accessibility.
+ */
+(function ($) {
+    $('.facets-widget-checkbox .facet-item').click(function(e) {
+        if (e.originalEvent !== undefined) {
+            $('label img', this).toggleClass('d-none');
+            $('input', this).click();
+        }
+    });
+})(jQuery);
