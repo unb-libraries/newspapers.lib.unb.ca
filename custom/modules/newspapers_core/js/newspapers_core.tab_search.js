56b235222c2c75c4db0a17d3bfdcaa8fc0f7994d
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2024-05-09 12:15:27 -0300

    NBNP-443 Document tabbed search form JS asset, remove Drupal dependency

--- custom/modules/newspapers_core/js/newspapers_core.tab_search.js
+++ custom/modules/newspapers_core/js/newspapers_core.tab_search.js
@@ -1,34 +1,34 @@
+/**
+ * @file
+ * Accessibility/usability improvements for front page tabbed search form.
+ */
 (function ($, Drupal) {
     'use strict';
 
-    Drupal.behaviors.multiSubmit = {
-        attach: function () {
-            // Autofocus the search input of clicked tab.
-            $('.nav-tabs a[data-toggle="tab"]').on("shown.bs.tab", function () {
-                $('#newspapers-core-homepage input[type="search"]:visible').focus();
-            });
+    // Autofocus the search input of clicked tab.
+    $('.nav-tabs a[data-toggle="tab"]').on("shown.bs.tab", function () {
+        $('#newspapers-core-homepage input[type="search"]:visible').focus();
+    });
 
-            // Simulate click function when user presses enter key.
-            $('input[type="search"]').on("keypress", function (event) {
-                if (event.key === 'Enter') {
-                    $('#newspapers-core-homepage input[type=submit]:visible').focus();
-                    $('#newspapers-core-homepage input[type=submit]:visible').click();
-                    return false;
-                }
-            });
+    // Simulate click function when user presses enter key.
+    $('input[type="search"]').on("keypress", function (event) {
+        if (event.key === 'Enter') {
+            $('#newspapers-core-homepage input[type=submit]:visible').focus();
+            $('#newspapers-core-homepage input[type=submit]:visible').click();
+            return false;
+        }
+    });
 
-            // Ensure fulltext submissions contain string else prevent submission & report validity.
-            $('form').submit(function(event) {
-                const search_fulltext = '#newspapers-core-homepage input[name="input_fulltext"]';
-                if ($(search_fulltext).is(':visible') === true) {
-                    $(search_fulltext).attr('required', true);
-                    if (this.checkValidity() === false) {
-                        $(search_fulltext).addClass('is-invalid').attr('aria-invalid', 'true');
-                        event.preventDefault();
-                        this.reportValidity();
-                    }
-                }
-            });
-        },
-    };
-})(jQuery, Drupal);
+    // Ensure fulltext submissions contain string else prevent submission & report validity.
+    $('form').submit(function(event) {
+        const search_fulltext = '#newspapers-core-homepage input[name="input_fulltext"]';
+        if ($(search_fulltext).is(':visible') === true) {
+            $(search_fulltext).attr('required', true);
+            if (this.checkValidity() === false) {
+                $(search_fulltext).addClass('is-invalid').attr('aria-invalid', 'true');
+                event.preventDefault();
+                this.reportValidity();
+            }
+        }
+    });
+})(jQuery);
