205d6d7ca19727385c7006bae32c23b97986d266
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2024-05-09 12:18:38 -0300

    NBNP-417 Remove Drupal behaviours for citation JS, fix dependencies

--- custom/modules/newspapers_core/js/newspapers_core.copy_citation.js
+++ custom/modules/newspapers_core/js/newspapers_core.copy_citation.js
@@ -8,39 +8,35 @@
 (function ($, Drupal) {
     'use strict';
 
-    Drupal.behaviors.copy_citation = {
-        attach: function () {
-            let copyButton= '#clipboardjs-button';
-            let citationModal = '#citation-modal';
-            function triggerPopover(popoverContent) {
-                let options = {
-                    content: popoverContent,
-                    delay: { 'show': 500 },
-                    html: true,
-                    placement: 'left',
-                    trigger: 'manual',
-                };
-                $(copyButton).popover(options).popover('show');
-                $(copyButton).blur(function () {
-                    $(this).popover('hide');
-                    // Destroy popover object so cycled keyboard focuses don't re-trigger.
-                    $(this).popover('dispose');
-                });
-            }
+    let copyButton= '#clipboardjs-button';
+    let citationModal = '#citation-modal';
+    function triggerPopover(popoverContent) {
+        let options = {
+            content: popoverContent,
+            delay: { 'show': 500 },
+            html: true,
+            placement: 'left',
+            trigger: 'manual',
+        };
+        $(copyButton).popover(options).popover('show');
+        $(copyButton).blur(function () {
+            $(this).popover('hide');
+            // Destroy popover object so cycled keyboard focuses don't re-trigger.
+            $(this).popover('dispose');
+        });
+    }
 
-            // https://clipboardjs.com/#events. See also Advanced Usage > Bootstrap Modals.
-            Drupal.clipboard = new ClipboardJS(copyButton, {
-                container: $(citationModal).get(0)
-            });
-            Drupal.clipboard.on('success', function (e) {
-                triggerPopover('<span class="fa-solid fa-clipboard-check fa-lg mr-1 text-success"></span>Citation copied');
-                // Need to clear selection & move focus back to button that triggered the copy action.
-                e.clearSelection();
-            });
+    // https://clipboardjs.com/#events. See also Advanced Usage > Bootstrap Modals.
+    Drupal.clipboard = new ClipboardJS(copyButton, {
+        container: $(citationModal).get(0)
+    });
+    Drupal.clipboard.on('success', function (e) {
+        triggerPopover('<span class="fa-solid fa-clipboard-check fa-lg mr-1 text-success"></span>Citation copied');
+        // Need to clear selection & move focus back to button that triggered the copy action.
+        e.clearSelection();
+    });
 
-            Drupal.clipboard.on('error', function (e) {
-                triggerPopover('<span class="fa-solid fa-circle-exclamation mr-1 text-danger"></span>Unable to copy citation');
-            });
-        },
-    };
+    Drupal.clipboard.on('error', function (e) {
+        triggerPopover('<span class="fa-solid fa-circle-exclamation mr-1 text-danger"></span>Unable to copy citation');
+    });
 })(jQuery, Drupal);
