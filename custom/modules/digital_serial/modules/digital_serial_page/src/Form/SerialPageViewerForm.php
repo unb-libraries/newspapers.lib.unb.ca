cb658b45e416bb39ce15aef6c3d28b3ee8dd85cd
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2024-05-09 09:33:37 -0300

    Code standards: remove comma

--- custom/modules/digital_serial/modules/digital_serial_page/src/Form/SerialPageViewerForm.php
+++ custom/modules/digital_serial/modules/digital_serial_page/src/Form/SerialPageViewerForm.php
@@ -268,7 +268,7 @@ class SerialPageViewerForm extends FormBase {
     SerialIssueInterface $digital_serial_issue,
     string $page_number,
     File $page_image_file,
-    string $image_download_path,
+    string $image_download_path
   ): array {
     // URL object for Parent publication.
     try {
