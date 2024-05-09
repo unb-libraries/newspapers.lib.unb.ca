2dfd8e8d574cac07ae52a1212385bd0699f54e77
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2024-05-09 12:12:46 -0300

    NBNP-443 Document required search input JS asset, remove Drupal dependency

--- custom/modules/newspapers_core/js/newspapers_core.search_input_required.js
+++ custom/modules/newspapers_core/js/newspapers_core.search_input_required.js
@@ -1,21 +1,22 @@
-(function ($, Drupal) {
-    Drupal.behaviors.searchInputRequired = {
-        attach: function() {
-            // Ensure required attribute present input of type 'search'.
-            const search_input = 'input[type="search"]';
-            const required = $(search_input).attr('required');
-            if (typeof required === 'undefined' && required !== true) {
-                $(search_input).attr('required', true);
-            }
-
-            // If form validity check fails then prevent submission & report.
-            $('form').submit(function(event) {
-                 if (this.checkValidity() === false) {
-                    $(search_input).addClass('is-invalid').attr('aria-invalid', 'true');
-                    event.preventDefault();
-                    this.reportValidity();
-                }
-            });
-        }
+/**
+ * @file
+ * Add required attribute on input elements of type 'search'.
+ * Use built-in HTML5 form validation reporting and apply BS4 failure style on failure.
+ */
+(function ($) {
+    // Ensure required attribute present input of type 'search'.
+    const search_input = 'input[type="search"]';
+    const required = $(search_input).attr('required');
+    if (typeof required === 'undefined' && required !== true) {
+        $(search_input).attr('required', true);
     }
-})(jQuery, Drupal);
+
+    // If form validity check fails then prevent submission & report.
+    $('form').submit(function(event) {
+         if (this.checkValidity() === false) {
+            $(search_input).addClass('is-invalid').attr('aria-invalid', 'true');
+            event.preventDefault();
+            this.reportValidity();
+        }
+    });
+})(jQuery);
