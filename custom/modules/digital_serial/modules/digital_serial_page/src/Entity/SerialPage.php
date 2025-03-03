b729919f7fece627c59575bde4e802e3c990e944
Jacob Sanford <jsanford@unb.ca>
2025-03-03 12:11:41 -0400

    Re-add page-storage-mover

--- custom/modules/digital_serial/modules/digital_serial_page/src/Entity/SerialPage.php
+++ custom/modules/digital_serial/modules/digital_serial_page/src/Entity/SerialPage.php
@@ -466,6 +466,7 @@ class SerialPage extends ContentEntityBase implements SerialPageInterface {
    * {@inheritDoc}
    */
   public function postSave(EntityStorageInterface $storage, $update = TRUE) {
+    $this->movePageImageToPermanentStorage(TRUE);
     $this->deleteDziFiles();
     $this->deletePdfFile();
     parent::postSave($storage, $update);
@@ -576,6 +577,57 @@ class SerialPage extends ContentEntityBase implements SerialPageInterface {
       }
     }
     return [];
+  }
+
+    /**
+   * {@inheritDoc}
+   */
+  public function movePageImageToPermanentStorage($move_file = TRUE, $verbose = FALSE) {
+    $file = $this->getPageImage();
+    $perm_storage_uri = $this->getPagePermImageStorageUri();
+
+    $file_uri = $file->getFileUri();
+    $stream_wrapper_manager = \Drupal::service('stream_wrapper_manager')->getViaUri($file_uri);
+    $abs_file_path = $stream_wrapper_manager->realpath();
+    // Do nothing if no URI or already in permanent storage.
+    if (
+      empty($perm_storage_uri) ||
+      $file->getFileUri() == $perm_storage_uri ||
+      $abs_file_path == false ||
+      !file_exists($abs_file_path)
+      ) {
+      // Something might be wrong. Doing stuff might make it worse.
+      if ($verbose) {
+        $id = $this->id();
+        if (empty($perm_storage_uri)) {
+          \Drupal::logger('digital_serial_page')->notice("No permanent storage URI found for page $id");
+        }
+        if ($file->getFileUri() == $perm_storage_uri) {
+          \Drupal::logger('digital_serial_page')->notice("Page $id already in permanent storage");
+        }
+        if ($abs_file_path == false) {
+          \Drupal::logger('digital_serial_page')->notice("File path for page $id is invalid");
+        }
+        if (!file_exists($abs_file_path)) {
+          \Drupal::logger('digital_serial_page')->notice("File $abs_file_path for page $id does not exist in the filesystem");
+        }
+      }
+      return;
+    }
+    $file_system = \Drupal::service('file_system');
+    $issue = $this->getParentIssue();
+    $issue->createStoragePath();
+
+    if ($move_file) {
+      $file_system->move(
+        $file_system->realpath($file->getFileUri()),
+        $this->getPagePermImageStoragePath()
+      );
+    }
+
+    $file->setFileUri($perm_storage_uri);
+    $file->setPermanent();
+    $file->save();
   }
 
 }
