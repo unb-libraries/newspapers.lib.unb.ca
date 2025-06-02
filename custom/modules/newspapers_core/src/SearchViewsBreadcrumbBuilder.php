b99c1eb06173c644cdfc95c09b5c1233f23b8dd1
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2025-06-02 09:11:15 -0300

    NBNP-427 Map view: override breadcrumb trail

--- custom/modules/newspapers_core/src/SearchViewsBreadcrumbBuilder.php
+++ custom/modules/newspapers_core/src/SearchViewsBreadcrumbBuilder.php
@@ -31,6 +31,7 @@ class SearchViewsBreadcrumbBuilder implements BreadcrumbBuilderInterface {
         'view.digital_titles_lister.page_listing',
         'view.print_title_listing.page_listing',
         'view.digital_page_lister.page_issues',
+        'view.interactive_map.page_1',
       ]
     );
   }
