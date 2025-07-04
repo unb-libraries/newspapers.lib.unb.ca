9d2d01433231c48550f8327230aeea62bdf70133
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2025-07-04 10:26:51 -0300

    NBNP-410 Configure OpenSeadragon toolbar behavior

--- custom/modules/digital_serial/modules/digital_serial_page/js/digital_serial_page.openseadragon_viewer.js
+++ custom/modules/digital_serial/modules/digital_serial_page/js/digital_serial_page.openseadragon_viewer.js
@@ -1,7 +1,8 @@
 var viewer = OpenSeadragon({
-  id: "seadragon-viewer",
-  prefixUrl: "//openseadragon.github.io/openseadragon/images/",
-  tileSources: drupalSettings.digital_serial_page.tile_sources,
-  overlays: drupalSettings.digital_serial_page.overlays,
-  useCanvas: drupalSettings.digital_serial_page.use_canvas
+    id: "seadragon-viewer",
+    prefixUrl: "//openseadragon.github.io/openseadragon/images/",
+    tileSources: drupalSettings.digital_serial_page.tile_sources,
+    overlays: drupalSettings.digital_serial_page.overlays,
+    useCanvas: drupalSettings.digital_serial_page.use_canvas,
+    toolbar: "toolbarDiv"
 });
